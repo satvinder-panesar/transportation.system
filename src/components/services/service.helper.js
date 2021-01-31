1ee0aae129a48b23c07d365dc35f0907e1786284
Panesar, Satvinder Singh (US - Dallas) <spanesar@deloitte.com>
2021-01-31 14:42:22 -0600

    highlighting service item on selection

--- src/components/services/service.helper.js
+++ src/components/services/service.helper.js
@@ -1,3 +1,18 @@
 export function sortServices(data){
     return data.sort((x, y) => x.modeName > y.modeName ? (x.name > y.name ? 1: -1) : -1)
 }
+
+export function highlightService(lastSelectedServiceId, id, setLastSelectedServiceId){
+    if(lastSelectedServiceId === id){
+        return
+    }
+    if(lastSelectedServiceId !== -1){
+        let prevSelectedServiceEle = document.getElementById(`service${lastSelectedServiceId}`)
+        let prevSelectedServiceItemEle = prevSelectedServiceEle.firstChild
+        prevSelectedServiceItemEle.classList = []
+    }
+    setLastSelectedServiceId(id)
+    let currSelectedServiceEle = document.getElementById(`service${id}`)
+    let currSelectedServiceItemEle = currSelectedServiceEle.firstChild
+    currSelectedServiceItemEle.classList.add("selected")
+}
--- src/components/services/services.js
+++ src/components/services/services.js
@@ -2,12 +2,15 @@ import './services.scss'
 import useHttpClient from '../../common/http/useHttpClient'
 import {services} from '../../common/constants'
 import handleHttpStatus from '../../common/http/handleHttpStatus'
-import {sortServices} from './service.helper'
+import {sortServices, highlightService} from './service.helper'
 import ServiceItem from '../serviceItem/serviceItem'
+import {useState} from 'react'
 
 function Services(){
 
     let { data, status } = useHttpClient({url: services.URL})
+
+    const [lastSelectedServiceId, setLastSelectedServiceId] = useState(-1)
 
     let retVal = handleHttpStatus(status)
     if(retVal){
@@ -19,7 +22,7 @@ function Services(){
     return (
         <div id="services">
             {data && sortServices(data).map((service, index)=>
-                <div key={`service${index}`} id={`service${index}`}>
+                <div key={`service${index}`} id={`service${index}`} onClick={()=>highlightService(lastSelectedServiceId, index, setLastSelectedServiceId)}>
                     <ServiceItem key={`serviceItem${index}`} service={service}></ServiceItem>
                 </div>
             )}
